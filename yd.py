9797963e304331cae59db5206ec45c6e0f145313
Ilia Zubov <new.world.participant@gmail.com>
2025-04-16 15:17:44 +0200

    Date time parsing added

--- yd.py
+++ yd.py
@@ -10,6 +10,7 @@ print("Client created")
 with client:
     cur_dir = os.path.dirname(__file__)
     df = pd.read_csv(f"{cur_dir}/user_actions.csv")
+    df["datetime"] = pd.to_datetime(df["datetime"], format="%d-%m-%Y %H:%M:%S")
     print("Readed csv file...")
     df.to_excel(f"{cur_dir}/user_actions.xlsx", index=False)
     print("Converted .csv to .xlsx file")
